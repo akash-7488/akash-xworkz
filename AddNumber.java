2b5bd3fed5c9c549f46fe75203ff5fdb245d2693
akash nispate <akashn.xworkz@gmail.com>
2021-02-01 23:54:01 +0530

    java datatype files

---
+++ AddNumber.java
@@ -0,0 +1,34 @@
+class AdditionOfTwoNumbers
+{
+public static void main(String a[])
+{
+add(90,90);
+mul(80,80);
+div(70,70);
+sub(60,60);
+sub(60,70);
+}
+
+public static void add(int a, int b){
+int c=a+b;
+System.out.println(c);
+}
+
+public static void sub(int d, int e){
+int f=d-e;
+System.out.println(f);
+}
+
+public static void div(int g, int h){
+int i=g/h;
+System.out.println(i);
+}
+
+public static void mul(int j, int k){
+int l=j*k;
+System.out.println(l);
+}
+
+
+
+}
